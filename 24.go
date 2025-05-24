a6a920fbb89ab58ca17cd4eb9e53187ceda16d3b
Meet Gandhi <meetgandhi4041@gmail.com>
2025-05-24 21:18:55 +0530

    24.go

---
+++ 24.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// func task(id int) {
+// 	fmt.Println("Id:", id)
+// }
+
+func main() {
+	// Goroutines
+
+	fmt.Println("hi")
+	for i := 0; i <= 10; i++ {
+		// go task(i)
+
+		go func(i int) {
+			fmt.Println(i)
+		}(i) // for inline goroutines
+	}
+	time.Sleep(time.Second * 2)
+
+}
+
+// Commmand to run this file -
+// go run 24.go
